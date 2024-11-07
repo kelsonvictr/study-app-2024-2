c4b16f39e59731e8fc017e6e3a60f46fd9642aed
kelsonalmeida <kelson.victr@gmail.com>
2024-11-07 20:31:38 -0300

    inicio firebase

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,13 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+
+const firebaseConfig = {
+  
+};
+
+
+const app = !getApps.length ? initializeApp(firebaseConfig) : getApp();
+
+export const auth = getAuth(app)
+export const db = getFirestore(app)
